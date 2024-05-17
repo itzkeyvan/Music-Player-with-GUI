d57f5fe96efb9d6d512ec176ac021f058ae88261
itzkeyvan <itzkeyvan@gmail.com>
2024-05-17 22:28:33 +0330

    ListenerPanel done.

--- Music Player Graphical Project-Phase 2/src/main/java/graphic/musicplayergraphicalprojectphase2/Main.java
+++ Music Player Graphical Project-Phase 2/src/main/java/graphic/musicplayergraphicalprojectphase2/Main.java
@@ -3,21 +3,44 @@ package graphic.musicplayergraphicalprojectphase2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class Main extends Application
+{
+    static private Stage stage;
+    static private Scene scene;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
+        Image logo=new Image("file:src/main/resources/graphic/musicplayergraphicalprojectphase2/PngAndJpg/Spotify_App_Logo.svg.png");
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("playBar.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Music PLayer");
+        stage.getIcons().add(logo);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static Stage getStage() {
+        return stage;
+    }
+
+    public static void setStage(Stage stage) {
+        Main.stage = stage;
+    }
+
+    public static Scene getScene() {
+        return scene;
+    }
+
+    public static void setScene(Scene scene) {
+        Main.scene = scene;
     }
 }
---
+++ Music Player Graphical Project-Phase 2/src/main/java/view/ListenerPanelController.java
@@ -0,0 +1,63 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class ListenerPanelController {
+
+    @FXML
+    private HBox HBoxForFollowingArtists_ListenerPanel;
+
+    @FXML
+    private HBox HBoxForPlaylists_ListenerPanel;
+
+    @FXML
+    private Button btn_explorePremium;
+
+    @FXML
+    private Button btn_newPlaylist;
+
+    @FXML
+    private Label lbl_Credit;
+
+    @FXML
+    private Label lbl_Email;
+
+    @FXML
+    private Label lbl_FavouriteGenres;
+
+    @FXML
+    private Label lbl_FirstAndLastName;
+
+    @FXML
+    private Label lbl_Password;
+
+    @FXML
+    private Label lbl_PhoneNumber;
+
+    @FXML
+    private Label lbl_PremiumExpirationDate;
+
+    @FXML
+    private Label lbl_PremiumLeftDays;
+
+    @FXML
+    private Label lbl_PremiumPlan;
+
+    @FXML
+    private Label lbl_UserName;
+
+    @FXML
+    void explorePremiumBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void newPlaylistBtnClicked(MouseEvent event) {
+
+    }
+
+}
