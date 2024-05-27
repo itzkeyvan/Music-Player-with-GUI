9bc4b4b8448ac8f2aa4e7a4afe4f35de0e86e88d
itzkeyvan <itzkeyvan@gmail.com>
2024-05-27 14:31:55 +0330

    CreateNewPlayListController done.

---
+++ Music Player Graphical Project-Phase 2/src/main/java/view/CreateNewPlayListController.java
@@ -0,0 +1,54 @@
+package view;
+
+import exceptions.FreeAccountLimitException;
+import graphic.musicplayergraphicalprojectphase2.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+public class CreateNewPlayListController {
+
+    @FXML
+    private Label lblBTn_CancelCreatingPlayList;
+
+    @FXML
+    private Label lblBTn_CreatePlatList;
+
+    @FXML
+    private TextField txtField_PlayListName;
+
+    @FXML
+    void cancelCreatingPlayListLblBtn_Clicked(MouseEvent event)
+    {
+        Stage stage = (Stage) lblBTn_CancelCreatingPlayList.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void createPlayListLblBtn_Clicked(MouseEvent event)
+    {
+        try {
+            Main.getListenerController().newPlayList(txtField_PlayListName.getText());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText(null);
+            alert.setContentText("Playlist: "+txtField_PlayListName.getText()+" created successfully.");
+            Main.setLoggedIn(true);
+            Stage alertStage = (Stage) alert.getDialogPane().getScene().getWindow();
+            alertStage.getIcons().add(new Image("file:src/main/resources/graphic/musicplayergraphicalprojectphase2/PngAndJpg/PlayBar/Tick.png"));
+            alert.showAndWait();
+        } catch (FreeAccountLimitException e) {
+            System.out.println(e.getMessage());
+        }
+        Stage stage = (Stage) lblBTn_CreatePlatList.getScene().getWindow();
+        stage.close();
+    }
+}
